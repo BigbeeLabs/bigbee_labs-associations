87d68ddd7446cd9251aac8d211d55dca1480e765
joseph-bigbee <joseph@bigbeelabs.com>
2018-05-24 19:38:14 -0700

    upgrading poro has many

--- lib/bigbee_labs/associations/class_methods/poro_has_many.rb
+++ lib/bigbee_labs/associations/class_methods/poro_has_many.rb
@@ -3,18 +3,21 @@ module BigbeeLabs
     module ClassMethods
       module PoroHasMany
         def poro_has_many(class_sym, options={})
-          klass_name = options[:class_name]
-          klass_name ||= class_sym.to_s.singularize.camelize
+          klass_name = options[:class_name] || class_sym.to_s.singularize.camelize
 
           define_method(class_sym) do 
             klass = klass_name.constantize
-            collection = nil
             if options[:as]
               as_id = "#{options[:as]}_id".to_sym
               as_type = "#{options[:as]}_type".to_sym
-              collection = klass.where(as_id => self.id, as_type => self.class.name)
+              if options[:polymorphic]
+                return klass.where(as_id => self.id, as_type => self.class.name)
+              else
+                return klass.where(as_id => self.id)
+              end
+            else
+
             end
-            collection
           end
 
         end
